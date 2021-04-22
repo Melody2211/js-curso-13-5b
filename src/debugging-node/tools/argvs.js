46951eee0e4cea28c3eecfb060f928f8718f012a
A19B82289 educacion <A19B82289@educacion.edu.do>
2021-04-22 12:58:28 -0500

    exportar tools componets

---
+++ src/debugging-node/tools/argvs.js
@@ -0,0 +1,18 @@
+//argumentos procesados.
+const yargs = requiere('yargs');
+const {hidebin} = requiere('yargs/helpers');
+const argv_all = yargs(hidebin(process.argv))
+const options = requiere(' ./options');
+
+//agregamos los nuevos comandos a nuestra aplicacion CLI
+
+const argv = argv_all
+    .command ('create' , 'create new task' , options.optionsCreate)
+    .command ('update' , 'actualizar con completed' , options. optionsUpdate)
+    .command ('delete' , 'delete task' , options.optionsErase)
+    .help()
+    .argv;
+
+    // exportamos argv para poder usarlo en todo mi proyecto
+    module.exports = { argv }
+    
